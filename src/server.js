efe30fc28c51dda1f76bdba9daa97c8de6f42d09
AnaMasFlaviaMoraes <anaflavia.chamoraes@gmail.com>
2025-04-23 14:55:48 -0300

    Criando layout básico das telas

--- src/server.js
+++ src/server.js
@@ -10,7 +10,71 @@ app.set('views', path.join(__dirname, '../src', 'views'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
-    res.send("Hello world!");
+    const users = [
+        {
+            id: 1,
+            nome: "Ana Flávia",
+            username: "anamasflavia",
+            perfil: "admin",
+            telefones: [{ numero: "(11) 99999-9999", principal: true }],
+            emails: [{ endereco: "ana@email.com", principal: true }]
+        },
+        {
+            id: 2,
+            nome: "Carlos Silva",
+            username: "carlossilva",
+            perfil: "cliente",
+            telefones: [{ numero: "(21) 98888-7777", principal: true }],
+            emails: [{ endereco: "carlos@email.com", principal: true }]
+        }
+    ];
+    res.render('listar-usuarios', { users });
+});
+
+app.get('/editar', (req, res) => {
+    const user = {
+        id: 1,
+        nome: 'João Silva',
+        username: 'joaosilva',
+        cpf: '123.456.789-00',
+        perfil: 'CLIENTE',
+        telefones: [
+            { numero: '(11) 91234-5678', principal: true },
+            { numero: '(11) 97654-3210', principal: false }
+        ],
+        emails: [
+            { endereco: 'joao@email.com', principal: true },
+            { endereco: 'jsilva@outra.com', principal: false }
+        ]
+    };
+    res.render('editar-usuario', { user });
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/cadastro', (req, res) => {
+    res.render('criar-usuario');
+});
+
+app.get('/detalhes', (req, res) => {
+    const user = {
+        id: 1,
+        nome: 'João Silva',
+        username: 'joaosilva',
+        cpf: '123.456.789-00',
+        perfil: 'CLIENTE',
+        telefones: [
+            { numero: '(11) 91234-5678', principal: true },
+            { numero: '(11) 97654-3210', principal: false }
+        ],
+        emails: [
+            { endereco: 'joao@email.com', principal: true },
+            { endereco: 'jsilva@outra.com', principal: false }
+        ]
+    };
+    res.render('detalhar-usuario', { user });
 });
 
 app.listen(PORT, () => {
